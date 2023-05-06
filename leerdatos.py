19821a215502418e82369d1b42de240b99059914
Cristian't <cristian.gutierrez.t@mail.pucv.cl>
2023-05-06 18:41:38 -0400

    test

---
+++ leerdatos.py
@@ -0,0 +1,9 @@
+import serial
+
+# Configuración de la comunicación serial
+ser = serial.Serial('/dev/ttyACM0', 9600) # Se utiliza el puerto serial /dev/ttyUSB0 a una velocidad de 9600 baudios
+
+while True:
+    # Leer datos desde Arduino
+	line = ser.readline().decode('utf-8').strip()
+	print(line)
